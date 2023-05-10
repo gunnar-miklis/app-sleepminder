234855904f60cb28e42f38fd8c84d32e211e0374
gunnar-miklis <gunnar.miklis@gmail.com>
2023-05-10 12:53:58 +0200

    Add: figma css

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,16 @@
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import StartPage from './pages/StartPage';
+import WelcomePage from './pages/WelcomePage';
 
 function App() {
 	return (
-		<div className='app'>
-		</div>
+		<main>
+			<Routes>
+				<Route path='/start' element={<StartPage />} />
+				<Route path='/welcome' element={<WelcomePage />} />
+			</Routes>
+		</main>
 	);
 }
 
