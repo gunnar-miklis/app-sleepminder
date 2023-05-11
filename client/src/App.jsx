00cc5e11f39c4b02cacce2077a68c3e1e9cb30ae
gunnar-miklis <gunnar.miklis@gmail.com>
2023-05-11 11:31:03 +0200

    Rename: "start page" to "loading page" and restructure collaboration section to "welcome page"

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
-import Start from './pages/Start';
+import Loading from './pages/Loading';
 import Welcome from './pages/Welcome';
 import SetupStepOne from './pages/setup/Step1';
 import SetupStepTwo from './pages/setup/Step2';
@@ -10,7 +10,7 @@ function App() {
 	return (
 		<main>
 			<Routes>
-				<Route path="/start" element={<Start />} />
+				<Route path="/loading" element={<Loading />} />
 				<Route path="/welcome" element={<Welcome />} />
 				<Route path="/setup/step-1" element={<SetupStepOne />} />
 				<Route path="/setup/step-2" element={<SetupStepTwo />} />
--- client/src/pages/Welcome.jsx
+++ client/src/pages/Welcome.jsx
@@ -10,7 +10,10 @@ function Welcome() {
 				<Link to="/setup/step-1" className="btn-lg">Get started</Link>
 			</div>
 			<div>
-				<h1 className="brand-title"><span>Sleep</span>Minder</h1>
+				<h1>Cross-Class Collaboration</h1>
+				<p><a href="" target="_blank">Name</a> (data)</p>
+				<p><a href="" target="_blank">Alex</a>, <a href="" target="_blank">Gabriela</a>, <a href="" target="_blank">Marta</a> (ux/ui)</p>
+				<p><a href="" target="_blank">Gunnar</a> (webDev)</p>
 			</div>
 		</div>
 	);
