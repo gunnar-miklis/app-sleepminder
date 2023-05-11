5b8c7db4b108f8bb907b73e86e3f61d5b9387db9
gunnar-miklis <gunnar.miklis@gmail.com>
2023-05-11 19:26:43 +0200

    Restructure: home in components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,17 +6,17 @@ import SetupStepOne from './components/setup/Step1';
 import SetupStepTwo from './components/setup/Step2';
 import SetupStepThree from './components/setup/Step3';
 import SetupStepFour from './components/setup/Step4';
-import Home from './pages/Home';
+import UserAreas from './pages/UserAreas';
 import Setup from './pages/Setup';
 
 function App() {
 	return (
-		<main className=''>
+		<main>
 			<Routes>
 				<Route path="/loading" element={<Loading />} />
 				<Route path="/welcome" element={<Welcome />} />
 				<Route path="/setup" element={<Setup />} />
-				<Route path="/home" element={<Home />} />
+				<Route path="/home" element={<UserAreas />} />
 			</Routes>
 		</main>
 	);
---
+++ client/src/components/areas/Home.jsx
@@ -0,0 +1,13 @@
+import ReminderCard from "../design/cards/ReminderCard";
+import UserSmCard from "../design/cards/UserSmCard";
+
+function Home() {
+	return (
+		<>
+			<UserSmCard />
+			<ReminderCard />
+		</>
+	)
+}
+
+export default Home;
