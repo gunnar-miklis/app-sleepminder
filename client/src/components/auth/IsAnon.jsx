64d3d0927ee02a5610a4cd2d5726b817d1ca9e03
gunnar-miklis <gunnar.miklis@gmail.com>
2023-05-16 18:24:44 +0200

    Implement: Spinner

--- client/src/components/auth/IsAnon.jsx
+++ client/src/components/auth/IsAnon.jsx
@@ -1,11 +1,12 @@
 import { useContext } from 'react';
 import { AuthContext } from '../../context/auth.context';
 import { Navigate } from 'react-router-dom';
+import Spinner from '../Spinner';
 
 export default function IsAnon( { children } ) {
 	const { isLoggedIn, isLoading } = useContext( AuthContext );
 
-	if ( isLoading ) return <p>Loading...</p>;
+	if ( isLoading ) return <Spinner />;
 
 	if ( isLoggedIn ) {
 		return <Navigate to="/dashboard" />;
--- client/src/components/auth/IsPrivate.jsx
+++ client/src/components/auth/IsPrivate.jsx
@@ -1,11 +1,12 @@
 import { useContext } from 'react';
 import { AuthContext } from '../../context/auth.context';
 import { Navigate } from 'react-router-dom';
+import Spinner from '../Spinner';
 
 export default function IsPrivate( { children } ) {
 	const { isLoggedIn, isLoading } = useContext( AuthContext );
 
-	if ( isLoading ) return <p>Loading ...</p>;
+	if ( isLoading ) return <Spinner />;
 
 	if ( !isLoggedIn ) {
 		return <Navigate to="/login" />;
