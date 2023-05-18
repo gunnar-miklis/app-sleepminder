b3afdda22de11507f383ea4342d0d0e9e10c1611
gunnar-miklis <gunnar.miklis@gmail.com>
2023-05-18 11:19:28 +0200

    Minor Updates

--- server/middleware/jwt-expired.middleware.js
+++ server/middleware/jwt-expired.middleware.js
@@ -1,5 +1,4 @@
 function jwtExpired( err, req, res, next ) {
-	
 	if ( err.name === 'UnauthorizedError' && err.message === 'jwt expired' ) {
 		res.status( 401 ).json( { error: 'token expired' } );
 		return;
