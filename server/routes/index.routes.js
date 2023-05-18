055f451174ffde1d13a684381a4ab36002c4c726
gunnar-miklis <gunnar.miklis@gmail.com>
2023-05-18 15:03:03 +0200

    Deploy

--- server/routes/index.routes.js
+++ server/routes/index.routes.js
@@ -1,8 +1,8 @@
 const router = require( 'express' ).Router();
 
 router.get( '/', ( req, res, next ) => {
-	// res.json( 'All good in here' );
-	res.send( '<img src=\'https://i.kym-cdn.com/photos/images/original/001/401/347/312.jpg\' alt=\'all good in here\'/>' );
+	res.json( 'All good in here' );
+	// res.send( '<img src=\'https://i.kym-cdn.com/photos/images/original/001/401/347/312.jpg\' alt=\'all good in here\'/>' );
 } );
 
 module.exports = router;
