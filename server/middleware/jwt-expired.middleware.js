c38b72bf44698542393c9a6a957e827f2fe014ff
gunnar-miklis <gunnar.miklis@gmail.com>
2023-05-18 18:11:20 +0200

    Revert

--- server/middleware/jwt-expired.middleware.js
+++ server/middleware/jwt-expired.middleware.js
@@ -1,8 +1,8 @@
-// function jwtExpired( err, req, res, next ) {
-// 	if ( err.name === 'UnauthorizedError' && err.message === 'jwt expired' ) {
-// 		res.status( 401 ).json( { error: 'token expired' } );
-// 		return;
-// 	}
-// }
+function jwtExpired( err, req, res, next ) {
+	if ( err.name === 'UnauthorizedError' && err.message === 'jwt expired' ) {
+		res.status( 401 ).json( { error: 'token expired' } );
+		return;
+	}
+}
 
-// module.exports = { jwtExpired };
+module.exports = { jwtExpired };
