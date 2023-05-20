45ba8209438af252a04a96de77fbd6ba9a9d73e0
gunnar-miklis <gunnar.miklis@gmail.com>
2023-05-20 11:56:16 +0200

    update collabs

--- client/src/pages/Welcome.jsx
+++ client/src/pages/Welcome.jsx
@@ -24,12 +24,12 @@ function Welcome() {
 					<h1>Cross-Class Collaboration</h1>
 					<p>
 						<a href="https://www.linkedin.com/in/angelica-blanco/" target="_blank" rel="noreferrer">Angélica Blanco</a>,
-						<a href="https://www.linkedin.com/in/juan-giussani/" target="_blank" rel="noreferrer">Juan A. Giussani</a>
+						 <a href="https://www.linkedin.com/in/juan-giussani/" target="_blank" rel="noreferrer">Juan A. Giussani</a>
 						 (data)</p>
 					<p>
 						<a href="" target="_blank">Alex</a>,
-						<a href="https://www.linkedin.com/in/gabcarneiro" target="_blank" rel="noreferrer">Gabriela Carneiro</a>,
-						<a href="" target="_blank">Marta</a>
+						 <a href="https://www.linkedin.com/in/gabcarneiro" target="_blank" rel="noreferrer">Gabriela Carneiro</a>,
+						 <a href="" target="_blank">Marta</a>
 						 (ux/ui)</p>
 					<p>
 						<a href="https://flowcv.me/gunnar-miklis" target="_blank" rel="noreferrer">Gunnar Miklis</a>
