3367041310610fdf4ad85548b6854be9018aa29c
gunnar-miklis <gunnar.miklis@gmail.com>
2023-05-26 22:44:50 +0200

    Add: bug explaination

--- server/middleware/jwt-expired.middleware.js
+++ server/middleware/jwt-expired.middleware.js
@@ -1,3 +1,15 @@
+// BUG: when using my own "jwtExpired" middleware.
+//	this method works really great on localhost to delete expired tokens.
+//	but on the deployment server this crashes the app somehow.
+//	maybe, because there are other tokens stored as well?! idk.
+
+// NOTE: usage explained
+// if a token is expired, express-jwt will display an error message by default
+// to catch it, the very last middleware can do it with ( err, req, res, next )
+// if catched identify by certain keywords: UnauthorizedError and jwt expired
+// then sent an error message to the client.
+// the client then can validate and delete the token from the browser.
+
 // function jwtExpired( err, req, res, next ) {
 // 	if ( err.name === 'UnauthorizedError' && err.message === 'jwt expired' ) {
 // 		res.status( 401 ).json( { error: 'token expired' } );
